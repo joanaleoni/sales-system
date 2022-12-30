e12456087975c3f17a139535e0a9046e69d650d0
Joana Leoni Pereira <joanaleonipereira@gmail.com>
2022-12-30 20:02:56 -0300

    Getters and setters added

--- src/model/domain/Category.java
+++ src/model/domain/Category.java
@@ -14,6 +14,22 @@ public class Category {
         this.description = description;
     }   
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
     @Override
     public String toString() {
         return description;
